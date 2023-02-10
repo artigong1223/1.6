24b033a843e6b938c9c13eb6709f33afe4473c81
artigong1223 <121706621+artigong1223@users.noreply.github.com>
2023-02-10 22:01:42 +0800

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,34 +1,42 @@
-const headerButton = document.querySelectorAll('.header__button')
 const modalFeedBack = document.querySelector('.modal-feedback')
 const modalCall = document.querySelector('.modal-call')
 const modalOff = document.querySelectorAll('.modal-off')
-const icCheckstatus = document.querySelector('.ic-checkstatus')
-const icRepair = document.querySelector('.ic-repair')
 const bodyOverflow = document.body
-
-headerButton[1].addEventListener('click', () => {
+const icChat = document.querySelectorAll('.ic-chat')
+const icCall = document.querySelectorAll('.ic-call')
+const modalCallOff = document.querySelector('.modal-call-off')
+const modalChatOff = document.querySelector('.modal-chat-off')
+const form = document.querySelector('form')
+icChat.forEach(g => g.addEventListener('click', () => {
+    modalChatOff.classList.add('modal-on')
     modalFeedBack.classList.add('modal-on')
     bodyOverflow.classList.add('body-scroll-off')
-})
+}))
 modalOff[1].addEventListener('click', () => {
+    modalChatOff.classList.remove('modal-on')
     modalFeedBack.classList.remove('modal-on')
     bodyOverflow.classList.remove('body-scroll-off')
 })
-icCheckstatus.addEventListener('click', () => {
-    modalFeedBack.classList.add('modal-on')
-    bodyOverflow.classList.add('body-scroll-off')
-})
-
-
-headerButton[0].addEventListener('click', () => {
+icCall.forEach(g => g.addEventListener('click', () => {
+    modalCallOff.classList.add('modal-on')
     modalCall.classList.add('modal-on')
     bodyOverflow.classList.add('body-scroll-off')
-})
+}))
 modalOff[0].addEventListener('click', () => {
     modalCall.classList.remove('modal-on')
     bodyOverflow.classList.remove('body-scroll-off')
+    modalCallOff.classList.remove('modal-on')
 })
-icRepair.addEventListener('click', () => {
-    modalCall.classList.add('modal-on')
-    bodyOverflow.classList.add('body-scroll-off')
+modalCallOff.addEventListener('click', () => {
+    modalCall.classList.remove('modal-on')
+    bodyOverflow.classList.remove('body-scroll-off')
+    modalCallOff.classList.remove('modal-on')
 })
+modalChatOff.addEventListener('click', () => {
+    modalFeedBack.classList.remove('modal-on')
+    bodyOverflow.classList.remove('body-scroll-off')
+    modalChatOff.classList.remove('modal-on')
+})
+
+
+
