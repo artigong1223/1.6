716558e88d2c62fdc396ae7240fc0d10e5d85ee1
artigong1223 <121706621+artigong1223@users.noreply.github.com>
2023-02-10 22:01:02 +0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,19 @@ import services from './services.js';
 import menuBurger from './menuBurger.js';
 import modal from './modal.js';
 import sectionContent from './sectionContent.js';
+
+import Swiper, {Pagination} from 'swiper';
+Swiper.use([Pagination])
+new Swiper('.image-slider', {
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+  },
+  breakpoints: {
+      768: {
+          simulateTouch: false,
+          grabCursor: false,
+          touchRatio: 0
+      }
+  }
+  });
